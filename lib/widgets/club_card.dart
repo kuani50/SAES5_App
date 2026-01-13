a47e1574ed156372e26c4fff3c34d218e475fbd5
enzo <enzo@unicaen.fr>
2026-01-13 14:43:20 +0100

    fix : fix flutter

--- lib/widgets/club_card.dart
+++ lib/widgets/club_card.dart
@@ -20,7 +20,7 @@ class ClubCard extends StatelessWidget {
         border: Border.all(color: Colors.grey.shade200),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.05),
+            color: Colors.black,
             blurRadius: 10,
             offset: const Offset(0, 4),
           ),
